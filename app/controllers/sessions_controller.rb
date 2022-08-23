a492da08a8f5683f29734f176a2b7f820ef02bb3
Irakli Asatiani <ekeushka@gmail.com>
2022-08-23 11:45:57 +0400

    create authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,26 @@ class SessionsController < ApplicationController
   def new
     render 'login'
   end
+
+  def create
+    username = user_params[:username]
+    password = user_params[:password]
+
+    @user = User.find_by(username: username)
+
+    if @user and @user.authenticate(password)
+      flash[:notice] = 'Welcome!'
+      session[:user_id] = @user.id
+      redirect_to(root_path)
+    else
+      flash[:error] = "Username or password is incorrect"
+      render 'login'
+    end
+  end
+
+  private 
+
+  def user_params 
+    params.permit(:username, :password)
+  end
 end
