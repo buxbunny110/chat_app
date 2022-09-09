798b4c6d1d20c07eee4257351bb2bcde57ead98f
Irakli Asatiani <ekeushka@gmail.com>
2022-09-09 14:55:45 +0400

    create channel

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -4,4 +4,18 @@ class ChatroomController < ApplicationController
   def index
     @messages = Message.all
   end
+
+  def create 
+    message = Message.new(chatroom_params)
+
+    if message.save
+      ActionCable.server.broadcast 'chat_room', message: message.body
+    end
+  end
+
+  private 
+
+  def chatroom_params 
+    params.require(:chatroom).permit(:body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
+  post 'chatroom', to: 'chatroom#create'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'login', to: 'sessions#destroy'
+  mount ActionCable.server, at: '/cable'
 end
