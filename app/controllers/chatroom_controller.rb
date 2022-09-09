cf5d6e7f547567f1b0e627b75e52df03a0e4f20e
Irakli Asatiani <ekeushka@gmail.com>
2022-09-09 15:45:50 +0400

    create message pass it through channel and append in view

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,20 +2,24 @@ class ChatroomController < ApplicationController
   before_action :require_user
 
   def index
+    @message = Message.new
     @messages = Message.all
   end
 
   def create 
-    message = Message.new(chatroom_params)
-
+    message = current_user.messages.build(chatroom_params)
     if message.save
-      ActionCable.server.broadcast 'chat_room', message: message.body
+      ActionCable.server.broadcast 'chatroom_channel', body: render_message(message)
     end
   end
 
   private 
 
   def chatroom_params 
-    params.require(:chatroom).permit(:body)
+    params.require(:message).permit(:body)
+  end
+
+  def render_message(message)
+    render(partial: 'messages/message', locals: { message: message })
   end
 end
